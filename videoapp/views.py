3cde4052b64e2ddd0cc05b9fe874129805005dc8
joelraj.varma9@gmail.com <joelraj.varma9@gmail.com>
2019-09-30 20:59:52 -0700

    integrated allauth

--- videoapp/views.py
+++ videoapp/views.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
+@login_required
 def videofeed(requests):
-    return HttpResponse('Hello')
+
+    resp = HttpResponse('Hello')
+    return resp
--- videowebapp/settings.py
+++ videowebapp/settings.py
@@ -27,17 +27,34 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'customusers.CustomUser'
 
 # Application definition
 
 INSTALLED_APPS = [
+
+    #installedapps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    
+    #thirdpartyapps  
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    #customapps
+    'videoapp',
+    'customusers',
+
+
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -114,8 +131,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+LOGIN_REDIRECT_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+# LOGIN_URL = '/'
